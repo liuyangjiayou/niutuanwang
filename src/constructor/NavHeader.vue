d7877e4706be59c6b0dbbdbc6c65ea92bf36e645
451629802 <451629802@qq.com>
2017-11-20 14:03:32 +0800

    搭建vue环境完成并且议案在axios与qs

---
+++ src/constructor/NavHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <!--这是页面的最上面的头部导航-->
+      <nav>
+        <div class="nav-header-map">
+          <img src="">
+          <span>北京</span>
+        </div>
+        <div>
+          你好，请登录
+          <span>免费注册</span>
+        </div>
+        <ul>
+          <li>我的订单</li>
+          <li>我的牛团</li>
+          <li>企业采购</li>
+          <li>客户服务</li>
+          <li>网站导航</li>
+          <li>手机牛团</li>
+        </ul>
+      </nav>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {}
+        },
+        components: {},
+        methods: {}
+    }
+</script>
+<style scoped>
+  .nav-header-map{
+    width: 100%;
+    height: 40px;
+    border-bottom: 1px solid #f1f1f3;
+  }
+</style>
